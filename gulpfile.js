c82148f6b1b13c731ca8982c5246729f41749fd2
tjjjia <14137521+tjjjia@users.noreply.github.com>
2018-04-05 17:40:11 +0100

    dist versions compiled

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ gulp.task('inlinesource', function () {
 	var options = {
 		compress: true
 	};
-	return gulp.src('./src/coin-json.html')
+	return gulp.src('./src/coin-interactive-audio.html')
 		.pipe(inlinesource(options))
 		.pipe(gulp.dest('./dist'));
 });
